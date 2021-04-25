fc108d6ee89cafb4a3e29441c68bf75440245b56
Ken Wang <kenuiuc@gmail.com>
2021-04-25 19:07:55 +0800

    init

---
+++ src/main/java/com/ken/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.ken.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping(value = "/{orgname}/{appname}/users/{username}")
+	public String hello() throws Exception {
+		// will give a 500 code
+	    throw new Exception("dummy rest2 auth server error");
+	}
+
+}
